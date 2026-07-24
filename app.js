4aa031ddab8c7149834daa74663cc152a0b85c40
Sebastian <sebastianacosta1824@gmail.com>
2026-07-24 09:54:09 -0500

    Configuracion principal antes del ES

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-import express from 'express';
-
+const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get("/", (_, res) => {
-    res.send("Aprendiendo express,ficha 3407181");
+res.send('Aprendiendo express,ficha 3407181');
 });
 
 app.listen(port, () => {
-    console.log( `Servidor en funcionamiento en el puerto: ${port}`);
+console.log( `Servidor en funcionamiento en el puerto: `);
 });
