16942dee28660cbbe855a3bf837ea0745e8ad74d
Sebastian <sebastianacosta1824@gmail.com>
2026-07-24 11:47:08 -0500

    Configuracion de variables de entorno PARTE2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = 3000
+const port = process.env.PUERTO || 3000
 
 app.get("/", (_, res) => {
 res.send('Aprendiendo express,ficha 3407181, ADSO en el SENA');
