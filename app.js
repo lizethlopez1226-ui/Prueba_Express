d21ae412612066e7cd2ed1e537739dd44ef63359
sebastian <sebastian@gmail.com>
2026-07-31 10:27:27 -0500

    aplicacion configurando las variables de entorno

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+
 import express from 'express';
+//leer el archivo .env
+import {configDotenv} from "dotenv"
+configDotenv()
 
 const app = express();
-const port = process.env.PUERTO || 3000
+const port = process.env.PUERTO || 3030
 
 app.get("/", (_, res) => {
-res.send('Aprendiendo express,ficha 3407181, ADSO en el SENA');
+res.send('Aprendiendo express,ficha 3407181, programa ADSO, 31 de Julio');
 });
 
 app.listen(port, () => {
